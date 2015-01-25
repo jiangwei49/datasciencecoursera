801d7657269bb5544a9bb2b55de26ab5b329fff7
Wei Jiang <wei3jiang1@gmail.com>
2015-01-26 05:01:29 +0800

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,12 +1,16 @@
+# Read data
 testX <- read.table("./test/X_test.txt")
 testY <- read.table("./test/y_test.txt")
 trainX <- read.table("./train/X_train.txt")
 trainY <- read.table("./train/y_train.txt")
+usertest <- read.table("./test/subject_test.txt")
+usertrain <- read.table("./train/subject_train.txt")
+fx <- read.table("./features.txt")
+
+# combine data
 data <- rbind(testX, trainX)
 activity <- rbind(testY, trainY)
 colnames(activity) <- "activity"
-usertest <- read.table("./test/subject_test.txt")
-usertrain <- read.table("./train/subject_train.txt")
 user <- rbind(usertest,usertrain)
 colnames(user) <- "user"
 library(plyr)
@@ -14,13 +18,17 @@ library(dplyr)
 xx <- data
 xx <- cbind(xx,activity)
 xx <- cbind(xx,user)
-fx <- read.table("./features.txt")
-colnames(xx) <- c(as.character(fx$V2),"mean","sd","activity","user")
 
+# rename columns
+colnames(xx) <- c(as.character(fx$V2),"activity","user")
+
+# remove duplicated columns
 yy <- xx[which(!duplicated(colnames(xx)))]
 yy2 <- select(yy, contains("mean"))
 yy3 <- select(yy, contains("std"))
 yy <- cbind(yy2,yy3,activity,user)
+
+# rename the values for "activity"
 for (i in 1:10299) {
 y <- yy$activity[i]
   if (y==1) {
@@ -39,6 +47,8 @@ y <- yy$activity[i]
     print(yy$activity[i])
   }
 }
+
+
 library(reshape2)
 dMelt <- melt(yy,id=c("activity","user"))
 dMelt1 <- group_by(dMelt,activity,user,variable)
